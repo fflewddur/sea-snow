eadc95202995056eebabe834e7ed0a0746810859
Todd Kulesza <todd@dropline.net>
2020-03-08 12:12:57 -0700

    add testing and error handling logic

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
+
+// SnowData holds a summary of local weather conditions
+type SnowData struct {
+	Snowing bool
+	Testing bool
+}
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
@@ -11,8 +18,31 @@ func main() {
 
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
+		snowing := false
+		testing := false
+
+		params := r.URL.Query()
+		if params.Get("testSnow") != "" {
+			snowing = true
+			testing = true
+		} else if params.Get("testNoSnow") != "" {
+			testing = true
+		}
+
+		data := SnowData{
+			Snowing: snowing,
+			Testing: testing,
+		}
+
+		err := tmpl.Execute(w, data)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, http.StatusText(500), 500)
+		}
 	})
 
-	http.ListenAndServe(":9990", nil)
+	err := http.ListenAndServe(":9990", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
