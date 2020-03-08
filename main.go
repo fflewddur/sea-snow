0f05cdfa8cbe539ee7d20be1b5f1204478d2dba6
Todd Kulesza <todd@dropline.net>
2020-03-08 11:43:19 -0700

    add static file serving

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, nil)
